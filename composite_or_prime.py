2905f6a61afdef0d1bf67cd66b27c56660531943
Tenshi <92758195+tensh1hx@users.noreply.github.com>
2021-10-25 13:13:54 +0100

    Update composite_or_prime.py

--- composite_or_prime.py
+++ composite_or_prime.py
@@ -3,13 +3,13 @@ y = 1
 dividers = []
 
 while True:
-    if y < x:
+    if y <= x**0.5:
         y += 1
     p = x / y
     if x % y == 0:
-        dividers.append(y)
-    if x <= y:
-        if dividers == [1, x]:
+        dividers.extend([y,p])
+    if x**0.5 < y:
+        if len(dividers) < 3 or x < 3:
             print(x, "is a prime number!")
         else:
             print(x, "is a composite number!")
